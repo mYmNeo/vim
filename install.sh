703140c33faf6f2751691814d648156cacb74001
thomassong <thomassong@tencent.com>
2016-04-18 10:41:00 +0800

    add update script
    Signed-off-by: thomassong <thomassong@tencent.com>

--- install.sh
+++ install.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
 vim +PluginInstall +qall
+bin=$(dirname "${BASH_SOURCE[0]}")
+bin=$(cd "$bin"; pwd)
+
+repos=$(find ${bin}/ -maxdepth 3 -name ".git")
+for repo in ${repos[@]}; do
+  cd $(dirname ${repo})
+  echo "sync $repo"
+  git pull
+  git submodule update --init --recursive
+done
